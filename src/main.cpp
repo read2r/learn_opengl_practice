52d4a8a9014f29b9dd9c477ac08b4e168aa3b857
read2r <read2r@gmail.com>
2022-04-27 18:51:58 +0900

    Hello Triangle Exercise 1

--- src/main.cpp
+++ src/main.cpp
@@ -82,32 +82,38 @@ int main(int argc, char** argv) {
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-         0.5f,  0.5f, 0.0f, // top right
-         0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f, // top left
+        // first triangle
+        0.5f,  0.5f, 0.0f,
+        0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
+        // second triangle
+        0.5f,  -0.5f, 0.0f,
+        -0.5f,  -0.5f, 0.0f,
+        -0.5f,  0.0f, 0.0f,
     };
 
+    /*
     unsigned int indices[] = {
         0, 1, 3,
         1, 2, 3
     };
+    */
 
     unsigned int VAO;
     unsigned int VBO;
-    unsigned int EBO;
+    //unsigned int EBO;
 
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    //glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
@@ -131,8 +137,8 @@ int main(int argc, char** argv) {
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         //glBindVertexArray(0);
 
         // check and call events and swap the buffers
@@ -142,7 +148,7 @@ int main(int argc, char** argv) {
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    //glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
     
     glfwTerminate();
