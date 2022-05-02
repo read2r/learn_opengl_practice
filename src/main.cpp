a5854b9004e78b843379cc9d9d6bf6bb768a947d
read2r <read2r@gmail.com>
2022-05-02 11:26:36 +0900

    More 3D

--- src/main.cpp
+++ src/main.cpp
@@ -35,6 +35,8 @@ int main(int argc, char** argv) {
         return -1;
     }
 
+    glEnable(GL_DEPTH_TEST);
+
     float vertices[] = {
         // F
          0.5f,  0.5f,  0.5f, 1.0f, 1.0f,
@@ -150,8 +152,6 @@ int main(int argc, char** argv) {
     //mShader.setInt("texture1", 0);
     //mShader.setInt("texture2", 1);
 
-    glEnable(GL_DEPTH_TEST);
-
     // render loop
     while(!glfwWindowShouldClose(window)) {
         // input
@@ -167,8 +167,19 @@ int main(int argc, char** argv) {
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+        glm::vec3 cubePositions[] = {
+            glm::vec3( 0.0f,  0.0f,  0.0f), 
+            glm::vec3( 2.0f,  5.0f, -15.0f), 
+            glm::vec3(-1.5f, -2.2f, -2.5f),  
+            glm::vec3(-3.8f, -2.0f, -12.3f),  
+            glm::vec3( 2.4f, -0.4f, -3.5f),  
+            glm::vec3(-1.7f,  3.0f, -7.5f),  
+            glm::vec3( 1.3f, -2.0f, -2.5f),  
+            glm::vec3( 1.5f,  2.0f, -2.5f), 
+            glm::vec3( 1.5f,  0.2f, -1.5f), 
+            glm::vec3(-1.3f,  1.0f, -1.5f)  
+        };
+
 
         glm::mat4 view = glm::mat4(1.0f);
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
@@ -179,11 +190,19 @@ int main(int argc, char** argv) {
         // render containter
         mShader.use();
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
-        int modelLocation = glGetUniformLocation(mShader.ID, "model");
+        for(int i = 0; i < 10; i++) {
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+
+            int modelLocation = glGetUniformLocation(mShader.ID, "model");
+            glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
+        }
+
         int viewLocation = glGetUniformLocation(mShader.ID, "view");
         int projectionLocation = glGetUniformLocation(mShader.ID, "projection");
-        glUniformMatrix4fv(modelLocation, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLocation, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projectionLocation, 1, GL_FALSE, glm::value_ptr(projection));
         //glBindVertexArray(0);
